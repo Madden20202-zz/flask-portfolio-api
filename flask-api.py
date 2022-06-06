439b45308fa8c4db8a334e6ddde7c39ed06ff411
austin <business.mocksock@gmail.com>
2022-06-06 17:31:44 -0500

    make basic hello world to test

--- flask-api.py
+++ flask-api.py
@@ -1,1 +1,11 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello/', methods=['GET', ['Post']])
+
+def welcome():
+    return "Hello World"
+
+if __name == '__main__':
+    app.run(host='0.0.0.0', port=420)
