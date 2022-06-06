bf7be650dc6298ace35cc52ceff50c931637c500
austin <business.mocksock@gmail.com>
2022-06-06 18:13:52 -0500

    fix route, debug and name of file

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'Post'])
+@app.route('/personalInformation/', methods=['GET', 'Post'])
 
 def welcome():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=420)
+    app.run(host='localhost', port=420, debug=True)
+
