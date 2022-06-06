56203d2b27758c38fb77fb8b67f9a49fc3acea9a
austin <business.mocksock@gmail.com>
2022-06-06 18:25:21 -0500

    set up the SQL Alchemy

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 # Entrypoint of app
 app = Flask(__name__)
 
+# Setup the SQL Alchemy
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:////portfolio.db'
+db = SQLAlchemy(app)
+
 if __name__ == '__main__':
     # this allows it to run in debug mode
     app.run(debug=True)
