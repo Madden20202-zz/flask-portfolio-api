3077858e32516b39296f421316a4466a6f7cc39b
austin <business.mocksock@gmail.com>
2022-06-06 18:06:42 -0500

    fix stray square bracket pair

--- flask-api.py
+++ flask-api.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', ['Post']])
+@app.route('/hello/', methods=['GET', 'Post'])
 
 def welcome():
     return "Hello World"
