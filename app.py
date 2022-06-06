dcb7b7a5b6f4ef0c91f9c69546f90231101112f6
austin <business.mocksock@gmail.com>
2022-06-06 18:22:49 -0500

    remove hello world work

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 # Entrypoint of app
 app = Flask(__name__)
 
-@app.route('/personalInformation/', methods=['GET', 'Post'])
-
-def welcome():
-    return "Hello World"
-
 if __name__ == '__main__':
     # this allows it to run in debug mode
     app.run(debug=True)
