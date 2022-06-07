2eb82f3e08eee1ab96b2aa82c0e23eab6382a6a1
austin <business.mocksock@gmail.com>
2022-06-06 19:12:02 -0500

    try to set it to one port

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     # this allows it to run in debug mode
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
 
 
 class PersonalInformation(Schema):
@@ -50,6 +50,10 @@ class Traits(Schema):
         characteristics = fields.Str(required=True)
         positive_habits = fields.Str(required=True)
         hobbies = fields.Str(required=True)
+
+# Now find a way to have it sent out
+
+
 # I dont think a relationship table is needed
 # since the point of it is to show correlation
 # between multiple points of data
